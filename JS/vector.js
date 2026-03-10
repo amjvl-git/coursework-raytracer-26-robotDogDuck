9348559538de736a79825874dd6178e29a250204
Denis Saracev <P2922929@LEC-ADMIN.dmu.ac.uk>
2026-03-10 14:48:45 +0000

    Finished vector.js maths
    Completed working on vector.js on the maths section.

--- JS/vector.js
+++ JS/vector.js
@@ -3,35 +3,78 @@ export class Vec3
 {
     constructor (x, y, z)
     {
-        this.x = x
-        this.y = y
-        this.z = z
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
+
+
 
     // Add other vector to this one and return the result
     add(other)
     {
-        return new Vec3(this.x + other.x, this.y + other.y, this.z + other.z)
+        return new Vec3(this.x + other.x, this.y + other.y, this.z + other.z);
     }
 
     // Subtract other vector from this one and return the result
-    minus(other){}
+    minus(other)
+    {
+        return new Vec3(this.x - other.x, this.y + other.y, this.z + other.z);
+    }
 
     // Multiply other vector by this one and return the result
-    multiply(other){}
+    multiply(other)
+    {
+        return new Vec3(this.x * other.x, this.y * other.y, this.z * other.z);
+    }
 
     // Scale this vector by the number scalar and return the result
-    scale(scalar){}
+    scale(scalar)
+    {
+        return new Vec3(this.x * scalar, this.y * scalar, this.z * scalar);
+    }
     
     // Calculate the dot product of this vector with the other and return the result
-    dot(other) {}
+    dot(other)
+    {
+        return new Vec3((this.y * other.z) - (this.z * other.y),
+        (this.z * other.x) - (this.x * other.z),
+        (this.x * other.z) - (this.y * other.x));
+    }
+
+    calculating_magnitude(other){
+        // Squares and defines the other xyz axis
+        let other_x_squared = Math.pow(other.x, 2);
+        let other_y_squared = Math.pow(other.y, 2);
+        let other_z_squared = Math.pow(other.z, 2);
+        // Squares and defines this xyz axis
+        let x_squared = Math.powe(this.x, 2);
+        let y_squared = Math.powe(this.y, 2);
+        let z_squared = Math.powe(this.z, 2);
+        // Adds and square roots the sum to give the magnitude
+        let magnitude_x = Math.sqrt(other_x_squared + x_squared);
+        let magnitude_y = Math.sqrt(other_y_squared + y_squared);
+        let magnitude_z = Math.sqrt(other_z_squared + z_squared);
+        return (magnitude_x + magnitude_y + magnitude_z)
+    }
 
     // Calculate and return the magnitude of this vector
-    magnitude() {}
+    magnitude(other)
+    {
+        return new Vec3(Math.sqrt(this.calculating_magnitude(other)));
+    }
     
     // Calculate and return the magnitude of this vector without the square root
-    magnitudeSquared() {}
+    magnitudeSquared(other)
+    {
+        return new Vec3(this.calculating_magnitude(other));
+    }
 
     // Return a normalised version of this vector
-    normalised() {}
+    normalised(other)
+    {
+        let magnitude = Math.sqrt(this.calculating_magnitude(other))
+        let normal = 1 / magnitude
+        return new Vec3(this.x * normal, this.y * normal, this.z * normal);
+    }
 }
